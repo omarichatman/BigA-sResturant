6fa9603f7acb540f93b324a819242d8467352912
Omari <ochatman@umich.edu>
2017-10-15 17:38:07 -0400

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+// /**
+//  * Created by Omari Chatman on 6/14/2017.
+//  */
+// jQuery(window).scroll(function(){
+//     var fromTopPx = 200; // distance to trigger
+//     var scrolledFromtop = jQuery(window).scrollTop();
+//     if(scrolledFromtop > fromTopPx){
+//         jQuery('html').addClass('scrolled');
+//     }else{
+//         jQuery('html').removeClass('scrolled');
+//     }
+// });
